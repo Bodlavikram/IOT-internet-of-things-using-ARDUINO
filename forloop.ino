07bdba297f97df3184222d76d42e209c0306ba0b
Bodlavikram <Bodlavikram@users.noreply.github.com>
2017-09-02 08:46:56 +0530

    Add files via upload

---
+++ forloop.ino
@@ -0,0 +1,25 @@
+int led=5;
+
+
+void setup() {
+  
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+//Serial.println("enter the value");
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<=10;i++)
+  {
+    digitalWrite(led,HIGH);
+    Serial.println(i);
+    delay(100);
+    digitalWrite(led,LOW);
+    delay(100);
+    
+  }
+}
